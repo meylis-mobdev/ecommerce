1fe610098273ebea3e23ef4f815d3131be6d9fe9
SuperACER <gurbanmyradowMeylis280@gmail.com>
2025-03-24 19:13:49 +0500

    first commit

---
+++ lib/core/theme/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Color primary_100 = Color(0xFF8E6CEF);
+  Color primary_50 = Color(0xFFEFEAF5);
+  Color black_100 = Color(0xFF272727);
+  Color black_50 = Color(0xFF858484);
+  Color white_100 = Color(0xFFffffff);
+  Color white_50 = Color(0xFF858484);
+}
